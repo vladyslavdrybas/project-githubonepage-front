a0e25c4d225ea65cd36a0e20f500d69a71fe672e
Vladyslav Drybas <ukdvvw@gmail.com>
2024-04-09 18:49:05 +0200

    add signin stub

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Landing from "@/components/Landing";
 
 const Home: React.FunctionComponent = () => {
