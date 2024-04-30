a66f38ab05cae7c6ea96666ae68d18f1e1613740
Vladyslav Drybas <ukdvvw@gmail.com>
2024-04-30 20:22:02 +0200

    add faked testimonials data

---
+++ app/src/artifacts/faked.ts
@@ -0,0 +1,33 @@
+export const avatar = [
+  'https://cdn.midjourney.com/0f6417e6-27d7-4353-ab43-96d4045bfebe/0_1.webp',
+  'https://cdn.midjourney.com/de8879c2-f8e2-4720-a839-aa4b2d210e83/0_3.webp',
+  'https://cdn.midjourney.com/de8879c2-f8e2-4720-a839-aa4b2d210e83/0_2.webp',
+  'https://cdn.midjourney.com/de8879c2-f8e2-4720-a839-aa4b2d210e83/0_1.webp',
+  'https://cdn.midjourney.com/4367685e-751a-4733-aa56-1adca90f9db2/0_3.webp',
+  'https://cdn.midjourney.com/4367685e-751a-4733-aa56-1adca90f9db2/0_0.webp',
+  'https://cdn.midjourney.com/ec8a3c6d-bf2d-4bc8-aea5-2a66b96bb2f5/0_3.webp',
+  'https://cdn.midjourney.com/ec8a3c6d-bf2d-4bc8-aea5-2a66b96bb2f5/0_2.webp',
+  'https://cdn.midjourney.com/ec8a3c6d-bf2d-4bc8-aea5-2a66b96bb2f5/0_1.webp',
+  'https://cdn.midjourney.com/2be705c4-fee2-480a-a6a9-f24f5015a08a/0_2.webp',
+  'https://cdn.midjourney.com/2be705c4-fee2-480a-a6a9-f24f5015a08a/0_3.webp',
+  'https://cdn.midjourney.com/05773a81-c110-48b3-8453-72ab6d28490e/0_3.webp',
+  'https://cdn.midjourney.com/05773a81-c110-48b3-8453-72ab6d28490e/0_1.webp',
+];
+
+export const whiteLogos = [
+  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560628e8573c43893fe0ace_Sydney-white.svg',
+  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f4d520d0517ae8e8ddf13_Bern-white.svg',
+  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f46794c159024c1af6d44_Montreal-white.svg',
+  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e891fa22f89efd7477a_TerraLight.svg',
+  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560a09d1f6337b1dfed14ab_colorado-white.svg',
+  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f5caa77bf7d69fb78792e_Ankara-white.svg',
+];
+
+export const darkLogos = [
+  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560628889c3bdf1129952dc_Sydney-black.svg',
+  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f4d4d8b829a89976a419c_Bern-black.svg',
+  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f467502f091ccb929529d_Montreal-black.svg',
+  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e911fa22f2203d7514c_TerraDark.svg',
+  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560a0990f3717787fd49245_colorado-black.svg',
+  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f5ca4e548b0deb1041c33_Ankara-black.svg',
+];
